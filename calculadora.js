cad220a79a87fbc907cdd61c7b7a44ad9be768db
adrianvinuelas <adrian.vinuelas@gmail.com>
2016-05-19 23:24:58 +0200

    Version mejorada4 de calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -3,13 +3,13 @@ var num2;
 //var x = 0;
 //var resultado = 0;
 var numAcumulado;
-var operacion = "";
+var operacion = '';
 var numMemo;
 var encendido;
 var noAns;
 
 function asignarnum(num) {
-    "use strict";
+    'use strict';
     if ($('#disp span').html() === '0') {//para si se reincia con "c", no salga el 0 a la izquierda
         $('#disp span').html('');
     }
@@ -18,7 +18,7 @@ function asignarnum(num) {
 }
 
 function numOperando(acumulado) {
-    "use strict";
+    'use strict';
     var num, esFloat;
     if (acumulado !== 0) {
         esFloat = acumulado.split(".");
@@ -34,52 +34,52 @@ function numOperando(acumulado) {
 }
 
 function opera(op) {
-    "use strict";
-    console.log("entra en opera pork se a pulsado una operacion");
+    'use strict';
+    console.log('entra en opera pork se a pulsado una operacion');
     if (num1 === 0 || ((num1 !== 0) && noAns)) {
         num1 = numOperando(numAcumulado);
-        console.log("num1 en opera = " + num1);
+        console.log('num1 en opera = ' + num1);
     }
     operacion = op;
-    if (op === "suma") {
+    if (op === 'suma') {
         numAcumulado = $('#disp span').html() + '+';
-    } else if (op === "mult") {
+    } else if (op === 'mult') {
         numAcumulado = $('#disp span').html() + 'x';
-    } else if (op === "div") {
+    } else if (op === 'div') {
         numAcumulado = $('#disp span').html() + '/';
-    } else if (op === "resta") {
+    } else if (op === 'resta') {
         numAcumulado = $('#disp span').html() + '-';
     }
     $('#disp span').html(numAcumulado);
 }
 
 function igual() {
-    "use strict";
-    var resultado = 0 , numeros;
-    console.log("operacion en igual es = " + operacion);
-    if (operacion === "suma") {
+    'use strict';
+    var resultado = 0, numeros;
+    console.log('operacion en igual es = ' + operacion);
+    if (operacion === 'suma') {
         numeros = $('#disp span').html().split('+');
         num2 = numOperando(numeros[1]);
-        console.log("num1 = " + num1);
-        console.log("num2 = " + num2);
+        console.log('num1 = ' + num1);
+        console.log('num2 = ' + num2);
         resultado = num1 + num2;
-    } else if (operacion === "mult") {
+    } else if (operacion === 'mult') {
         numeros = $('#disp span').html().split('x');
         num2 = numOperando(numeros[1]);
-        console.log("num1 = " + num1);
-        console.log("num2 = " + num2);
+        console.log('num1 = ' + num1);
+        console.log('num2 = ' + num2);
         resultado = num1 * num2;
-    } else if (operacion === "div") {
+    } else if (operacion === 'div') {
         numeros = $('#disp span').html().split('/');
         num2 = numOperando(numeros[1]);
-        console.log("num1 = " + num1);
-        console.log("num2 = " + num2);
+        console.log('num1 = ' + num1);
+        console.log('num2 = ' + num2);
         resultado = num1 / num2;
-    } else if (operacion === "resta") {
+    } else if (operacion === 'resta') {
         numeros = $('#disp span').html().split('-');
         num2 = numOperando(numeros[1]);
-        console.log("num1 = " + num1);
-        console.log("num2 = " + num2);
+        console.log('num1 = ' + num1);
+        console.log('num2 = ' + num2);
         resultado = num1 - num2;
     }
     $('#disp span').html(resultado);
@@ -89,19 +89,20 @@ function igual() {
 }
 
 function verOper(num) {
+    'use strict';
     if (num === 43) {
-        return "suma";
-    } else if(num === 45) {
-        return "resta";
-    } else if(num === 47) {
-        return "div";
-    } else if(num === 120) {
-        return "mult";
+        return 'suma';
+    } else if (num === 45) {
+        return 'resta';
+    } else if (num === 47) {
+        return 'div';
+    } else if (num === 120) {
+        return 'mult';
     }
 }
 
 function verTecla(key) {
-    "use strict";
+    'use strict';
     var numero, op;
     if (key > 47 && key < 58) {//es un numero
         numero = key - 48; //48 es por el caracter del 0 en el teclado, 49(1)-48(0) = 1;
@@ -115,14 +116,14 @@ function verTecla(key) {
             opera(op);
         }
     } else if (key === 13 || key === 61) {
-		igual();
+        igual();
     } else if (key === 99 || key === 67) {
         $('#disp span').html('0');
-        num1=0;
-        num2=0;
-        numAcumulado=0;
+        num1 = 0;
+        num2 = 0;
+        numAcumulado = 0;
     } else if (key === 77 || key === 109) {
-        if (operacion !== "") {
+        if (operacion !== '') {
             asignarnum(numMemo);
         } else {
             $('#disp span').html('');
@@ -131,53 +132,54 @@ function verTecla(key) {
     }
 }
 
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
+    'use strict';
     numAcumulado = 0;
     num1 = 0;
     num2 = 0;
     numMemo = 0;
     noAns = true;
-    $('#calculadora a').click(function(){
+    $('#calculadora a').click(function () {
         if (encendido) {
             if ($(this).html() === '+') {
-                opera("suma");
-            } else if($(this).html() === 'x') {
-                opera("mult");
-            } else if($(this).html() === '/') {
-                opera("div");
-            } else if($(this).html() === '-') {
-                opera("resta");
-            } else if($(this).html() === '=') {
+                opera('suma');
+            } else if ($(this).html() === 'x') {
+                opera('mult');
+            } else if ($(this).html() === '/') {
+                opera('div');
+            } else if ($(this).html() === '-') {
+                opera('resta');
+            } else if ($(this).html() === '=') {
                 igual();
-            } else if($(this).html() === 'c') {
-                $('#disp span').html("0");
-                num1=0;
-                num2=0;
-                //asignarnum("0");
-            } else if($(this).html() === "mem") {
+            } else if ($(this).html() === 'c') {
+                $('#disp span').html('0');
+                num1 = 0;
+                num2 = 0;
+                //asignarnum('0');
+            } else if ($(this).html() === 'mem') {
                 if ($('#disp span').html() === 'ans') {
                     numMemo = num1;
-                    console.log("numMemo desde ans = " + numMemo);
+                    console.log('numMemo desde ans = ' + numMemo);
                 } else {
                     numMemo = $('#disp span').html();
-                    console.log("numMemo = " + numMemo);
+                    console.log('numMemo = ' + numMemo);
                 }
-            } else if ($(this).html() === "m") {
-                if (operacion !== "") {
+            } else if ($(this).html() === 'm') {
+                if (operacion !== '') {
                     asignarnum(numMemo);
                 } else {
                     $('#disp span').html('');
                     asignarnum(numMemo);
                 }
-            } else if ($(this).html() === "on/off") {
+            } else if ($(this).html() === 'on/off') {
                 encendido = false;
                 $('#disp span').html('Good bye!');
-                $("#disp span").addClass("apagar");
-                setTimeout(function() { 
-                    $("#disp span").removeClass("apagar");
-                    $('#disp span').html('');}, 
+                $('#disp span').addClass('apagar');
+                setTimeout(function () { 
+                    $('#disp span').removeClass('apagar');
+                    $('#disp span').html(''); }, 
                 3000);
-            } else if ($(this).html() === "ans") {
+            } else if ($(this).html() === 'ans') {
                 noAns = false;
                 num1 = numOperando($('#disp span').html());
                 $('#disp span').html('ans');
@@ -185,11 +187,11 @@ jQuery(document).ready(function() {
                 asignarnum($(this).html());
             }
         } else {
-            if ($(this).html() === "on/off") {
+            if ($(this).html() === 'on/off') {
                 encendido = true;
-                asignarnum("0");
+                asignarnum('0');
             }
-        }	
+        }
     });
 
     $(window).keypress(function(e) {
